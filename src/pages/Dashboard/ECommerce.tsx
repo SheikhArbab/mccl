379969df469bd09aa62b420af641d8719106f72b
SheikhArbab <arbabzafar4444@gmail.com>
2024-07-01 02:57:13 +0500

    clear flowbit

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -1,12 +1,10 @@
 import { FC } from 'react'; 
 import { TableOne, MapOne, ChatCard, ChartTwo, ChartThree, ChartOne, CardDataStats } from "@/components/index"
-
-import { Datepicker } from "flowbite-react";
+ 
 
 const ECommerce: FC = () => {
   return (
-    <>
-      <Datepicker />
+    <> 
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
         <CardDataStats title="Total views" total="$3.456K" rate="0.43%" levelUp>
